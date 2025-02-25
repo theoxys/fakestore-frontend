d14a4cad041c266ecfd43e9fb97aaeee497eae17
Matheus Pires <ferreiramatheus48@gmail.com>
2025-02-25 18:39:01 -0300

    feat: add page transitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Link } from "next-view-transitions";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-background text-foreground">
       <h1>Hello World</h1>
+
+      <Link href="/admin">Admin</Link>
     </div>
   );
 }
